5b6d49e7b6a2c219d24f652f9347925d37c85592
Ovidiojcf <ojcfarias@gmail.com>
2025-04-20 19:42:57 -0300

    chore(tailwind): adiciona configuração inicial com cores personalizadas

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+export default {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          orange_web: '#faa916',
+          white_custom: '#fbfffe',
+          dim_gray: '#6d676e',
+          eerie_black: '#1b1b1e',
+          carmine: '#96031a',
+        }
+      }
+    },
+    plugins: []
+  }
+  
