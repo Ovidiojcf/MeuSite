d1528fdcf01e4b9e1d6b98a70103b55c99572961
Ovidiojcf <ojcfarias@gmail.com>
2025-04-20 21:01:03 -0300

    refactor(app): integra componente About à estrutura principal

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import '@/main.js'
 import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
+import About from './components/About.vue'
 </script>
 
 <template>
   <Navbar/>
   <Hero/>
+  <About/>
 </template>
 
 
