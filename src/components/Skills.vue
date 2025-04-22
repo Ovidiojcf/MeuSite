80eeebe56e8aad1766e247b14a1689cd03c1cd09
Ovidiojcf <ojcfarias@gmail.com>
2025-04-22 18:18:29 -0300

    feat(skills): centraliza rodapé de créditos e ajusta ícones e textos

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="px-4 py-16 bg-eerie-black text-white">
         <div class="max-w-6xl mx-auto">
-            <h2 class="text-orange-web text-3xl md:text-4xl font-bold mb-12 text-center">Minhas Habilidades</h2>
+            <h2 class="text-orange-web text-3xl md:text-5xl font-bold mb-12 text-center">Minhas Habilidades</h2>
 
             <div class="grid md:grid-cols-2 gap-12">
                 <!-- Hard Skills -->
@@ -17,7 +17,8 @@
                             <p class="mt-2 text-sm">CSS</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="w-12 h-12 object-contain mt-2" src="@/assets/skills/javascript-programming-language-icon.svg" alt="JavaScript">
+                            <img class="w-12 h-12 object-contain mt-2"
+                                src="@/assets/skills/javascript-programming-language-icon.svg" alt="JavaScript">
                             <p class="mt-2 text-sm">JavaScript</p>
                         </div>
                         <div class="flex flex-col items-center">
@@ -25,11 +26,13 @@
                             <p class="mt-2 text-sm">Vue</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="w-12 h-12 object-contain mt-2" src="@/assets/skills/tailwind-css-icon.svg" alt="Tailwind">
+                            <img class="w-12 h-12 object-contain mt-2" src="@/assets/skills/tailwind-css-icon.svg"
+                                alt="Tailwind">
                             <p class="mt-2 text-sm">Tailwind</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="w-12 h-12 object-contain mt-2" src="@/assets/skills/github-icon.svg" alt="GitHub">
+                            <img class="w-12 h-12 object-contain mt-2" src="@/assets/skills/github-icon.svg"
+                                alt="GitHub">
                             <p class="mt-2 text-sm">GitHub</p>
                         </div>
                         <div class="flex flex-col items-center">
@@ -48,30 +51,35 @@
                     <h3 class="text-2xl font-semibold mb-6 text-center">Soft Skills</h3>
                     <div class="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center">
                         <div class="flex flex-col items-center">
-                            <img class="text-orange-web w-12 h-12 object-contain mt-2" src="@/assets/skills/user-hard-work.svg" alt="">
+                            <i class="fi fi-rr-user-gear text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Esforçado</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="text-orange-400 w-12 h-12 object-contain mt-2" src="@/assets/skills/brain-circuit.svg" alt="">
+                            <i class="fi fi-ss-brain-circuit text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Mente curiosa</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="text-orange-web w-12 h-12 object-contain mt-2" src="@/assets/skills/assessment.svg" alt="">
+                            <i class="fi fi-br-assessment text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Detalhista</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="text-orange-web w-12 h-12 object-contain mt-2" src="@/assets/skills/people-group.svg" alt="">
+                            <i class="fi fi-bs-team-check text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Colaborador</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="w-12 h-12 object-contain mt-2 text-orange-web hover:text-carmine" src="@/assets/skills/arrows-down-curve.svg" alt="">
+                            <i class="fi fi-rr-shield-check text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Resiliência prática</p>
                         </div>
                         <div class="flex flex-col items-center">
-                            <img class="text-orange-web w-12 h-12 object-contain mt-2" src="@/assets/skills/sparkles.svg" alt="">
+                            <i class="fi fi-rr-sparkles text-orange-web text-5xl hover:text-carmine"></i>
                             <p class="mt-2 text-sm text-center">Catalisador de ideias</p>
                         </div>
                     </div>
+                    <p class="text-center mt-4">
+                        Uicons by <a href="https://www.flaticon.com/uicons"
+                            class="underline text-orange-web hover:text-carmine">Flaticon</a>
+                    </p>
+
                 </div>
             </div>
         </div>
